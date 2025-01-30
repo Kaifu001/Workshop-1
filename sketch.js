036d260d23411b3583875c4520d6a5f648df400c
Kaifu001 <kaifu.yang@icloud.com>
2025-01-30 17:05:26 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  noStroke(); 
+  background(220, 200, 180); 
 }
 
 function draw() {
-  background(220);
+  drawRandomShapes(); 
+}
+
+function drawRandomShapes() {
+  for (let i = 0; i < 10; i++) { 
+    let x = random(width);
+    let y = random(height);
+    let size = random(20, 50);
+    let r = random(100, 255);
+    let g = random(50, 200);
+    let b = random(150, 255);
+
+    fill(r, g, b);
+
+    if (random() < 0.5) { 
+      ellipse(x, y, size, size); 
+    } else {
+      rect(x, y, size, size); 
+    }
+  }
 }
